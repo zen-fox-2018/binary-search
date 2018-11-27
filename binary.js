9489a75c247ca54dd76fcccec6bdbdfd5044223a
Arief Rachman <arievrchman@gmail.com>
2018-11-27 23:33:24 +0700

    binary search dan insertion sort

--- binary.js
+++ binary.js
@@ -5,12 +5,40 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+  for (let i = 0; i < arr.length; i++) {
+    let temp = arr[i]
+    for (let j = i - 1; j >= 0 && arr[j] > temp; j--) {
+      if (temp < arr[j]) {
+        arr[j + 1] = arr[j]
+        arr[j] = temp
+      }
+    }
+  }
   return arr
 }
 
-function binarySearch (search, array) {
+function binary_search(search, array) {
   // Your searching code
-  return 0;
+  let kecil = 0;
+  let besar = array.length - 1;
+
+  while (kecil <= besar) {
+    let midPoint = Math.floor((besar + kecil) / 2);
+
+    if (array[midPoint] === search) {
+      debugger;
+      return midPoint
+    } else {
+      if (array[midPoint] > search) {
+        debugger;
+        besar = midPoint - 1
+      } else {
+        debugger;
+        kecil = midPoint + 1
+      }
+    }
+  }
+  return -1
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
