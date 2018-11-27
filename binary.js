800daa9f99c06ebb395f96c5f124a9e56b64a63f
irsyahms <irsyahmardiahs@gmail.com>
2018-11-27 10:11:45 +0700

    Add files via upload

---
+++ binary.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
+var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
+
+function ownSort(arr) {
+  // Your sorting code
+  return arr
+}
+
+function binarySearch (search, array) {
+  // Your searching code
+  return 0;
+}
+
+var arrayGenapSorted = ownSort(testArrayGenap)
+var arrayGanjilSorted = ownSort(testArrayGanjil)
+
+// Driver code
+console.log(binary_search(8, arrayGenapSorted))
+console.log(binary_search(10, arrayGenapSorted))
+console.log(binary_search(33, arrayGenapSorted))
+
+console.log(binary_search(53, arrayGanjilSorted))
+console.log(binary_search(3, arrayGanjilSorted))
+console.log(binary_search(2, arrayGanjilSorted))
+
+module.exports = {
+  binary_search
+}
