d63e49b55307d25902ede938109dec49f4c088d3
rachmadza <razato.zai@gmail.com>
2018-11-27 21:43:23 +0700

    push anchor binary search

--- binary.js
+++ binary.js
@@ -5,12 +5,54 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+  for (let i = 0; i < arr.length; i++) {
+
+    for (let j = i - 1; j >= 0; j--) {
+
+      if (arr[j + 1] < arr[j]) {
+
+        var temp = arr[j + 1]
+
+        arr[j + 1] = arr[j]
+
+        arr[j] = temp
+
+      }
+
+    }
+
+  }
+
   return arr
 }
 
-function binarySearch (search, array) {
+
+function binary_search(search, array) {
   // Your searching code
-  return 0;
+  var awal = 0
+  var akhir = array.length - 1
+
+  while (awal <= akhir) {
+
+    var tengah = Math.floor((awal + akhir) / 2)
+
+    if (search > array[tengah]) {
+
+      awal = tengah + 1
+
+    } else if (search < array[tengah]) {
+
+      akhir = tengah - 1
+
+    } else if (search == array[tengah]) {
+
+      return tengah
+    }
+
+  }
+
+  return -1
+
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
@@ -28,3 +70,23 @@ console.log(binary_search(2, arrayGanjilSorted))
 module.exports = {
   binary_search
 }
+
+
+/*
+#PSEUDOCODE
+STORE 'awal' with 0
+STORE 'akhir' with 'array' length minus 1
+WHILE 'awal' <= 'akhir'
+    STORE 'tengah' with 'awal' plus 'akhir' divided by 2
+    IF 'search' > 'array' with index 'tengah'
+        SET 'awal' with 'tengah'
+    ELSE IF 'search' < 'array' with index 'tengah'
+        SET 'akhir' with 'tengah'
+    ELSE IF 'search' === 'array' with index 'tengah'
+        RETURN 'tengah'
+    ELSE 
+        RETURN -1
+    ENDIF
+ENDWHILE
+RETURN -1
+*/
