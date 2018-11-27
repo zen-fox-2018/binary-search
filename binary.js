a81a1b6371f6f55da93777d83ba38aacdc44a258
ShadowM8 <anton2014lenovo@gmail.com>
2018-11-27 16:05:11 +0700

    binary.js done - anton-

--- binary.js
+++ binary.js
@@ -1,20 +1,85 @@
 'use strict'
 
+/*
+ALGORITMA
+1. pertama urutkan array yang akan dicari elemen indeksnya
+2. buat variabel awal sama dengan 0
+3. buat variabel akhir sama dengan panjang array - 1
+4. buat variabel mid dengan nilainya adalah (awal + akhir / 2) dibulatkan ke bawah
+5. lakukan loop for dengan indeks i sama dengan 0 sampai panjang array
+  5a. buat variabel itsExist sama dengan false , yang bertujuan untuk mengecek jika search(nilai yang dicari) ada di dalam target atau tidak ada
+  5b. lakukan loop for dengan indeks j sama dengan 0 sampai panjang array
+    5ba. jika search sama dengan array indeks j maka
+      5baa. itsExist sama dengan true
+  5c. Jika itsExist sama dengan true lakukan
+    5ca. selama array[mid] tidak sama dengan search lakukan
+      5caa. Jika array[mid] lebih kecil dari search, maka 
+        5caaa. akhir = array.length - 1
+        5caba. awal = mid + 1
+      5cab. selain itu, maka
+        5caba. awal = 0
+        5cabb. akhir = mid - 1
+      5cac. mid = Math.floor((awal + akhir) / 2)
+    5cb. kembalikan mid
+  5d. Selain itu, maka
+    5da. return -1
+ */
+
 var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
-function ownSort(arr) {
-  // Your sorting code
-  return arr
-}
-
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function ownSort(array) {
+  for (var i = 0; i < array.length; i++) {
+    var nilaiMinimum = array[i]
+    var indeks = i
+    for (var j = i; j < array.length; j++) {
+      if (nilaiMinimum > array[j]) {
+        nilaiMinimum = array[j]
+        indeks = j
+      }
+    }
+    var temp = array[i]
+    array[i] = array[indeks]
+    array[indeks] = temp
+  }
+  return array
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
+
+function binary_search(search, array) {
+  var awal = 0
+  var akhir = array.length - 1
+  var mid = Math.floor((awal + akhir) / 2)
+  for (var i = 0; i < array.length; i++) {
+    var itsExist = false
+    for (var j = 0; j < array.length; j++) {
+      if (search === array[j]) {
+        itsExist = true
+      }
+    }
+    if (itsExist === true) {
+      while (array[mid] !== search) {
+        if (array[mid] < search) {
+          akhir = array.length - 1
+          awal = mid + 1
+        }
+        else {
+          awal = 0
+          akhir = mid - 1
+        }
+        mid = Math.floor((awal + akhir) / 2)
+      }
+      return mid
+
+    }
+    else {
+      return -1
+    }
+  }
+
+}
 
 // Driver code
 console.log(binary_search(8, arrayGenapSorted))
